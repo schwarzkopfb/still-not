d169f574cbec4bc0043f35a0ac55adcf66f545ef
Balázs Schwarzkopf <schwarzkopfb@icloud.com>
2017-01-06 16:00:57 +0100

    debug coverage upload issue on Travis

--- bin/cover.sh
+++ bin/cover.sh
@@ -1,9 +1,8 @@
 PATH="$PATH:./node_modules/.bin"
 
 rm -rf ./coverage
-node test/basic.js &>/dev/null
-node test/irrelevant.js &>/dev/null
-istanbul cover ./test/custom-handler-before-require.js --report none --print none --include-pid &>/dev/null
-istanbul cover ./test/custom-handler-after-require.js --report none --print none --include-pid &>/dev/null
-cat ./coverage/lcov.info
+node test/basic.js
+node test/irrelevant.js
+istanbul cover ./test/custom-handler-before-require.js --report none --print none --include-pid
+istanbul cover ./test/custom-handler-after-require.js --report none --print none --include-pid
 istanbul report lcov
