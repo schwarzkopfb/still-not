57f7aca1d0738cf87255011f542fae6d705d4b00
Balázs Schwarzkopf <schwarzkopfb@icloud.com>
2017-01-06 15:54:09 +0100

    debug coverage upload issue on Travis

--- bin/cover.sh
+++ bin/cover.sh
@@ -6,3 +6,4 @@ node test/irrelevant.js &>/dev/null
 istanbul cover ./test/custom-handler-before-require.js --report none --print none --include-pid &>/dev/null
 istanbul cover ./test/custom-handler-after-require.js --report none --print none --include-pid &>/dev/null
 istanbul report lcov
+cat ./coverage/lcov.info
